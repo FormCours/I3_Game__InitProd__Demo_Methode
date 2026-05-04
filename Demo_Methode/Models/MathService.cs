09a6a484ffefcab2fbfb405f97945a0534360bda
cognitic <cognitic@UNITY-PCPROFF>
2026-05-04 14:51:46 +0200

    Add project files.

---
+++ Demo_Methode/Models/MathService.cs
@@ -0,0 +1,60 @@
+﻿namespace Demo_Methode.Models
+{
+    public struct MathService
+    {
+        // Méthode avec deux parametres
+        // - "nb1" : Obligatoire
+        // - "nb2" : Optionnel, avec la valeur par default de 1
+        public int Multi(int nb1, int nb2 = 1)
+        {
+            return nb1 * nb2;
+        }
+
+        // Surchage de méthode
+        // - Un méthode qui porte le meme nom
+        // - Mais avec un signature différente
+        // Soit le nombre de parametre change, soit le type parametre
+        public double Multi(double nb1, double nb2)
+        {
+            Console.WriteLine();
+            return 42;
+        }
+        public double Multi(double nb1, double nb2, double nb3)
+        {
+            return 42;
+        }
+
+
+        // Méthode qui prend au moins 2 parametres : 
+        // - "nb1" : Obligatoire
+        // - "nb2" : Obligatoire
+        // - "nbs" : Nombre indeterminé via "params" (Un seul)
+        public int Addition(int nb1, int nb2, params int[] nbs)
+        {
+            int result = nb1 + nb2;
+            foreach(int val in nbs)
+            {
+                result += val;
+            }
+            return result;
+        }
+
+
+        // Méthode avec un test de garde 
+        public double Division(int nb1, int nb2)
+        {
+            // Test de garde
+            if(nb2 == 0)
+            {
+                // - Retour d'une valeur adaptée
+                return double.PositiveInfinity; 
+                //     (Par default, le symbole pour l'infini c'est "8" en console)
+
+                // - Déclanchement d'une erreur
+                // throw new Exception("Boum !");
+            }
+
+            return (double)nb1 / nb2;
+        }
+    }
+}
---
+++ Demo_Methode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Demo_Methode.Models;
+
+// Support de l'UTF8 (Ca sert a rien, je veux mon emoji :p)
+Console.OutputEncoding = Encoding.UTF8;
+
+
+// Syntaxe en C# d'une méthode:
+// type_retour NomMethode(parametres)
+
+// ↓ Méthodes déclarés directement dans la zone programme
+bool EstPair(int nombre)
+{
+    return ((nombre / 2) * 2) == nombre;
+}
+
+string GetName()
+{
+    Console.WriteLine("Quel est votre prénom ?");
+    Console.Write("> ");
+    string name = Console.ReadLine()!;
+
+    return name;
+}
+
+void SayGoodBye()
+{
+    Console.WriteLine("Au revoir !");
+    Console.WriteLine("👋");
+}
+
+
+int val = 42;
+bool test = EstPair(val);
+Console.WriteLine($"La valeur {val} est {(test ? "pair" : "Impair")}");
+
+string prenom = GetName();
+Console.WriteLine($"Bien le bonjour {prenom}");
+SayGoodBye();
+
+
+MathService m = new MathService();
+int r1 = m.Multi(21, 2);  // 42
+int r2 = m.Multi(13);     // 13
+
+int r3 = m.Addition(1, 2, 3, 4, 5);
+
